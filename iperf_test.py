b56f8741962b92f3781621f5e37b310450e39b05
Daniel Himes <dhimes@gmail.com>
2024-01-09 12:31:30 -0500

    clean up

--- iperf_test.py
+++ iperf_test.py
@@ -5,14 +5,16 @@ url = 'http://192.168.0.154:8000/run-iperf'
 
 # Example iperf parameters - you can modify these based on your requirement
 server_ip = '192.168.0.42'
-duration = 1
+duration = 13
 
-# Construct the URL with query parameters
-url_with_params = f"{url}?server_ip={server_ip}&duration={duration}"
+params = {
+    'server_ip': server_ip,
+    'duration': duration
+}
 
-# Making a POST request to the iperf endpoint
-response = requests.post(url_with_params)
+response = requests.post(url, params=params)
 
 # Printing the response
 print("Status Code:", response.status_code)
 print("Response:", response.json())
+
